341fab62d48a2a99409a54d7d8b8f992dc92bdbb
Julia Souza <julia-souza@uergs.edu.br>
2024-09-08 20:33:27 -0300

    QR Code Project

---
+++ 18 - Node js/3 - QR Code Project/index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+})
+.catch((error) => {
+    if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+        console.log("Something else went wrong");
+    }
+});
