32afdfcc5ee254bc3fe2010291cd3a3346023650
Julia Souza <julia-souza@uergs.edu.br>
2024-09-08 19:30:29 -0300

    Update Node.js Native Modules exercise

---
+++ 18 - Node js/2 - NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyName";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
