21b8232ce0a4348c007648ba8578e534e84e623e
Julia Souza <julia-souza@uergs.edu.br>
2024-10-24 15:00:56 -0300

    Add EJS exercise

---
+++ 20 - EJS/1 -  EJS/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
