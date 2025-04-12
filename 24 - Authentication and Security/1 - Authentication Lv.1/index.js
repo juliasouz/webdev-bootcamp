a9d648b542b370527de18405d4ff64d75786d0b9
Julia Souza <julia-souza@uergs.edu.br>
2025-04-12 01:14:37 -0300

    Add project structure for secrets management application
    - Created package.json with necessary dependencies including express, dotenv, and passport.
    - Added header and footer partials for consistent layout across views.
    - Implemented home, login, register, and secrets views with appropriate forms and styling.
    - Included Bootstrap and Font Awesome for enhanced UI.
    - Styled the application with custom CSS for better visual appeal.

--- 24 - Authentication and Security/1 - Authentication Lv.1/index.js
+++ 24 - Authentication and Security/1 - Authentication Lv.1/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "fhce.X5wPC4KHMx",
-  port: 5433,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
--- 24 - Authentication and Security/2 - Authentication Lv.2/index.js
+++ 24 - Authentication and Security/2 - Authentication Lv.2/index.js
@@ -2,20 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "fhce.X5wP",
-  port: 5433,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
