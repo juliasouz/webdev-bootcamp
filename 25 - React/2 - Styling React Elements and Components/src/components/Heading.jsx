f37fd8be77ba27c880017dcfa35fa1e0f0734ade
Julia Souza <julia-souza@uergs.edu.br>
2025-04-17 17:02:00 -0300

    Updating React exercise with components for food list application
    - Implemented App component to render heading, food list, and images.
    - Created Heading component to display the main title.
    - Developed List component to show a list of favorite foods.
    - Set up index.jsx for rendering the main App component.

---
+++ 25 - React/2 - Styling React Elements and Components/src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+    return <h1 className="Heading">My Favorite Foods</h1>;
+}
+
+export default Heading;
---
+++ 25 - React/2 - Styling React Elements and Components/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+    return (
+        <ul className="food-list" contentEditable="true">
+            <li>Pizza</li>
+            <li>Vegetarian Sushi</li>
+            <li>Cheeseburgers</li>
+        </ul>
+    );
+}
+
+export default List;
