991f9c4ecd40cc195c8db82b4c0c0738b3e99683
Julia Souza <julia-souza@uergs.edu.br>
2025-04-17 18:42:18 -0300

    Refactor App component to move variable declarations inside the function scope for better encapsulation

--- 25 - React/2 - Styling React Elements and Components/src/components/App.jsx
+++ 25 - React/2 - Styling React Elements and Components/src/components/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Heading from "./Heading";
 import List from "./List";
 
-const fName = "Julia";
-const lName = "Souza";
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-
-const pizzaImg = "https://th.bing.com/th/id/R.8693c5c5f91aa52c0ecfc14e7044fe0b?rik=3%2b1hFItj2tAQDA&riu=http%3a%2f%2ffoodelhi.in%2fwp-content%2fuploads%2f2017%2f08%2fIMG_20170819_162616_187.jpg&ehk=dgetZA0wSUFW8ev0tfuaGi0Zn1aEHSqE%2fKbCX4OUsXk%3d&risl=&pid=ImgRaw&r=0";
-const sushiImg = "https://th.bing.com/th/id/OIP.OnrUtC-g06jB02kUkZIBRQHaHa?rs=1&pid=ImgDetMain";
-const burgerImg = "https://th.bing.com/th/id/OIP.7qc8sbej8hthhMR82H68FQHaHa?rs=1&pid=ImgDetMain";
-
 function App() {
+    const fName = "Julia";
+    const lName = "Souza";
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const pizzaImg = "https://th.bing.com/th/id/R.8693c5c5f91aa52c0ecfc14e7044fe0b?rik=3%2b1hFItj2tAQDA&riu=http%3a%2f%2ffoodelhi.in%2fwp-content%2fuploads%2f2017%2f08%2fIMG_20170819_162616_187.jpg&ehk=dgetZA0wSUFW8ev0tfuaGi0Zn1aEHSqE%2fKbCX4OUsXk%3d&risl=&pid=ImgRaw&r=0";
+    const sushiImg = "https://th.bing.com/th/id/OIP.OnrUtC-g06jB02kUkZIBRQHaHa?rs=1&pid=ImgDetMain";
+    const burgerImg = "https://th.bing.com/th/id/OIP.7qc8sbej8hthhMR82H68FQHaHa?rs=1&pid=ImgDetMain";
+    
     return (
         <>
             <div>
