1515e08c31cf214e539e266db07146138aaa7562
Julia Souza <julia-souza@uergs.edu.br>
2025-04-28 19:23:39 -0300

    Add React Devtools exercise with components for displaying contacts
    - Added package.json with dependencies and scripts for development and build.
    - Created styles.css for basic styling of the application.
    - Implemented App component to render a list of contacts with Avatar and Card components.
    - Developed Avatar component to display user images.
    - Created Card component to show contact details including name, phone, and email.
    - Added Detail component for rendering individual contact information.
    - Included contacts.js file with sample contact data.
    - Set up index.jsx for rendering the main App component.
    - Configured Vite for project build and development.

---
+++ 25 - React/9 - React Devtools/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://tse1.explicit.bing.net/th/id/OIP.tc66-3U0FbHVWah_w-KdkQHaHa?rs=1&pid=ImgDetMain" className="circle-img" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
