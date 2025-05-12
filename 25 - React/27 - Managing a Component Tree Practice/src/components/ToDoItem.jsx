080e073c005e8a0af2e2ff45b70aad6d58e403e5
Julia Souza <julia-souza@uergs.edu.br>
2025-05-12 18:12:20 -0300

    Add React exercise for managing a component tree practice
    - Added package.json with dependencies and scripts for development
    - Created styles.css for application styling
    - Implemented main App component to manage to-do items
    - Added InputArea component for user input
    - Created ToDoItem component to display individual to-do items
    - Set up index.jsx for rendering the App component
    - Configured Vite for project build and development

---
+++ 25 - React/27 - Managing a Component Tree Practice/src/components/ToDoItem.jsx
@@ -0,0 +1,13 @@
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default ToDoItem;
